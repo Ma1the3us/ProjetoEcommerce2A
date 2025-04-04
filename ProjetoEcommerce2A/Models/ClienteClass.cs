16d5b4e3020a55410cce3bd702e2cdce5e4bf3a4
Matheus&Marco <matheusrodriguesilva4@gmail.com>
2025-04-04 16:22:21 -0300

    criando as Models

--- ProjetoEcommerce2A/Models/ClienteClass.cs
+++ ProjetoEcommerce2A/Models/ClienteClass.cs
@@ -2,5 +2,10 @@
 {
     public class ClienteClass
     {
+        public int CodCli {get; set; }
+        public string? NomeCli { get; set; }
+        public string? TelCli { get; set; }
+        public string? EmailCli { get; set; }
+        public List<Cliente>? ListarCliente { get; set; }
     }
 }
